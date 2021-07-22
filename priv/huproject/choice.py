fd188f8736fb791181f8da9b75e49b184749bb0e
VENOM-InstantDeath <mohathepro111@gmail.com>
2021-07-22 20:45:52 -0300

    Added two new programs to huproject

---
+++ priv/huproject/choice.py
@@ -0,0 +1,15 @@
+import random
+
+def choice(s):
+    #Darth, Antites, Standby, Kain
+    #Output: Darth
+    s = [i.strip() for i in s.split(',')]
+    return random.choice(s)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        print(choice(sys.argv[1]))
+        exit(0)
+    s = input('list: ')
+    print(choice(s))
